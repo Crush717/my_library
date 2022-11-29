3c7be34c9bae155f135f42634d5c4cb2971850fd
Crush717 <3146891687@qq.com>
2022-11-29 23:07:11 +0800

    feat: 登录（部分）功能

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.template.context_processors import request
+from django.views import generic
 
-# Create your views here.
+from main.models import Reader
+
+
+def loginView(request):
+    """
+    登录页面
+    """
+    return render(request, 'main/login.html', {})
+
+
+def loginApi(request, phone_number, password):
+    """
+    登录
+    """
+    # 根据手机号获取读者
+    reader = Reader.objects.filter(phone_number=phone_number)\
+    # 密码
+
+    # 跳转到首页
+
+
+
